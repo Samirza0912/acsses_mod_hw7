5485f9bd80ea95e07774e9eec5200ac765ab5514
Samirza0912 <samirza@code.edu.az>
2022-03-18 17:18:58 +0400

    feat:i add homework7 about acsses modifiers

---
+++ access_mod/Models/UserClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace access_mod.Models
+{
+    class UserClass
+    {
+        
+        private string _Username;
+        private int _Age;
+        private object _Password;
+
+        public string username
+        {
+            get
+            {
+                return _Username;
+            }
+            set
+            {
+                //Console.WriteLine("Enter Your username: ");
+                //_Username = Console.ReadLine();
+                //Console.WriteLine(_Username);
+                _Username = value;
+                Console.WriteLine(_Username);
+
+            }
+
+        }
+        public int age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                //Console.WriteLine("Please enter your age");
+                //int v = int.Parse(Console.ReadLine());
+                //_Age = v;
+                
+                if (value < 0)
+                {
+                    Console.WriteLine("age cannot be negative number");
+                    return;
+                }
+                _Age = value;
+
+                Console.WriteLine(_Age);
+
+            }
+
+        }
+        public object password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                string sym = "bukayooooooo";
+                for (int i = 0; i < sym.Length; i++)
+                {
+                    if (sym.Length<8)
+                    {
+                        Console.WriteLine("Password cannot be less than 8");
+                        return;
+                    }
+                }
+                 _Password = value;
+                Console.WriteLine(_Password);
+            }
+
+        }
+        
+
+
+
+    }
+}
---
+++ access_mod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using access_mod.Models;
+
+namespace access_mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            UserClass user = new UserClass();
+            //user.age; user.username; user.password;
+            user.age = 20;
+            user.username = "bukayoooooo";
+            user.password = "Bukayoooooo7";
+            //user.username = "abcd";
+
+
+
+        }
+    }
+}
